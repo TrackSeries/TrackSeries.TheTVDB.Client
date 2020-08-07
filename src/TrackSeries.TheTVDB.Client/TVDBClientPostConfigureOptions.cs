8485bbfe000e8a59a908edc0ac417cfc6c264fc0
Carlos J. Aliaga <carlos@cjaliaga.net>
2020-08-07 21:48:15 +0200

    Trying to read configuration from IConfiguration if it's not configured manually.

--- src/TrackSeries.TheTVDB.Client/TVDBClientPostConfigureOptions.cs
+++ src/TrackSeries.TheTVDB.Client/TVDBClientPostConfigureOptions.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using TrackSeries.TheTVDB.Client.Languages;
 
 namespace TrackSeries.TheTVDB.Client
 {
     internal class TVDBClientPostConfigureOptions : IPostConfigureOptions<TVDBClientOptions>
     {
+        private readonly IConfiguration _configuration;
+
+        public TVDBClientPostConfigureOptions(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void PostConfigure(string name, TVDBClientOptions options)
         {
+            Initialize(options);
+
             if (string.IsNullOrEmpty(options.ApiKey))
             {
                 throw new InvalidOperationException("ApiKey must be configured.");
@@ -21,6 +30,19 @@ namespace TrackSeries.TheTVDB.Client
             if (string.IsNullOrEmpty(options.AcceptedLanguage))
             {
                 options.AcceptedLanguage = "en";
+            }
+        }
+
+        private void Initialize(TVDBClientOptions options)
+        {
+            //If there's no ApiKey let's try to read values from Configuration
+            if (string.IsNullOrEmpty(options.ApiKey))
+            {
+                var newOptions = _configuration.GetSection(nameof(TVDBClientOptions)).Get<TVDBClientOptions>();
+                options.ApiKey = newOptions.ApiKey;
+                options.AcceptedLanguage = newOptions.AcceptedLanguage;
+                options.ReturnCompleteUrlForImages = newOptions.ReturnCompleteUrlForImages;
+                options.ShareContextBetweenClients = newOptions.ShareContextBetweenClients;
             }
         }
     }
